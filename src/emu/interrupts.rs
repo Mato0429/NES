10c3f11e7235ebf7d4884daf307fdd061c691217
M4to0429 <91228634+Mato0429@users.noreply.github.com>
2026-03-05 09:45:37 +0000

    [add]  割り込みを管理する構造体の実装と改善
    レビューを反映し、ドキュメントとインターフェースを改善しました。

--- src/emu/interrupts.rs
+++ src/emu/interrupts.rs
@@ -1,38 +1,71 @@
-//! CPUの割り込みを管理するための構造体を定義するモジュールです。
+//! CPUの割り込み要求を管理するための構造体を定義するモジュールです。
 //!
 //! 参考: [bokuweb/flownes](https://github.com/bokuweb/flownes/blob/master/src/interrupts/index.js)
 
-/// CPUの割り込みを管理します。
+/// CPUの割り込み要求を管理します。
 ///
-/// 各コンポーネントが[`Rc<RefCell>`]で参照を持ってメソッドを呼び出し、
-/// CPUが割り込みを取得することで割り込みを処理します。
+/// 各コンポーネントが[`Rc<RefCell>`]からメソッドで割り込み要求を行い、
+/// CPUが要求を取得することで割り込みを処理します。
 pub struct Interrupts {
     nmi: bool,
     irq: bool,
 }
 
 impl Interrupts {
+    /// 電源投入時の初期状態を生成します。
+    ///
+    /// 割り込み要求はすべて解除された状態です。
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    /// 割り込み状態をリセットします。
+    ///
+    /// 割り込み要求はすべて解除されます。
+    pub fn reset(&mut self) {
+        self.deassert_nmi();
+        self.deassert_irq();
+    }
+
+    /// NMI割り込みを取得します。
     pub fn nmi(&self) -> bool {
         self.nmi
     }
 
+    /// NMI割り込みを取得します。
     pub fn irq(&self) -> bool {
         self.irq
     }
 
+    /// NMI割り込みを要求します。
     pub fn assert_nmi(&mut self) {
         self.nmi = true;
     }
 
-    pub fn asset_irq(&mut self) {
+    /// IRQ割り込みを要求します。
+    pub fn assert_irq(&mut self) {
         self.irq = true;
     }
 
+    // NMI割り込みを解除します。
     pub fn deassert_nmi(&mut self) {
         self.nmi = false;
     }
 
+    /// IRQ割り込みを解除します。
     pub fn deassert_irq(&mut self) {
         self.irq = false;
+    }
+}
+
+// bool::default()がfalseのため、Interruptsにderive(Default)できるが、
+// 仕様の変更や可読性を考えて値をそのまま書けるようにしておく
+#[allow(clippy::derivable_impls)]
+impl Default for Interrupts {
+    fn default() -> Self {
+        Self {
+            nmi: false,
+            irq: false,
+        }
     }
 }
